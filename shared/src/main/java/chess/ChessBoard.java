ca3ee30be501a92cf1087beb983e19292af0481f
dallinmj <dallinmjacobs@gmail.com>
2024-09-16 17:09:30 -0600

    Finished King and fixed board index 0

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -17,9 +17,8 @@ public class ChessBoard {
      * @param position where to add the piece to
      * @param piece    the piece to add
      */
-    public void addPiece(ChessPosition position, ChessPiece piece)
-    {
-        squares[position.getRow()][position.getColumn()] = piece;
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+        squares[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -30,7 +29,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        return squares[position.getRow()][position.getColumn()];
+        return squares[position.getRow() - 1][position.getColumn() - 1];
     }
 
     /**

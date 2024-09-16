1fa0dd102f8bbd5313efe7c4600d472de09ad943
dallinmj <dallinmjacobs@gmail.com>
2024-09-16 15:45:39 -0600

    Added first Bishop Test :)

--- shared/src/main/java/chess/BishopCalculator.java
+++ shared/src/main/java/chess/BishopCalculator.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /*
 Calculate the potential moves for the Bishop piece depending on its position and other pieces
@@ -11,62 +12,64 @@ public class BishopCalculator {
     public static Collection<ChessMove> potentialMoves(ChessBoard board, ChessPosition position){
         //Import needed info (Location, board)
         Collection<ChessMove> validMoves = new ArrayList<>();
+        List<int[]> coordinates = new ArrayList<>();
         int x = position.getColumn();
         int y = position.getRow();
 
         ChessMove move;
+        ChessPosition originalPosition = new ChessPosition(y, x);
 
         // Up & Right
-        int X = x; // temp
-        int Y = y; // temp
+        int original_x = x; // temp
+        int original_y = y; // temp
         while (x < 8 && y < 8) {
              x++;
              y++;
-             move = new ChessMove(new ChessPosition(x - 1, y - 1), new ChessPosition(x, y), null);
+             move = new ChessMove(originalPosition, new ChessPosition(y, x), null);
              validMoves.add(move);
-        }
-
-        Collection<tuple> coordinates = new ArrayList<>();
-
-        // Up & Left
-        x = X;
-        y = Y;
-        while (x > 0 && y < 8) {
-            x--;
-            y++;
-            move = new ChessMove(new ChessPosition(x + 1, y - 1), new ChessPosition(x, y), null);
-            validMoves.add(move);
+             coordinates.add(new int[]{y, x});
         }
 
         // Down & Right
-        x = X;
-        y = Y;
-        while (x < 8 && y > 0) {
+        x = original_x;
+        y = original_y;
+        while (x < 8 && y > 1) {
             x++;
             y--;
-            move = new ChessMove(new ChessPosition(x - 1, y + 1), new ChessPosition(x, y), null);
+            move = new ChessMove(originalPosition, new ChessPosition(y, x), null);
             validMoves.add(move);
+            coordinates.add(new int[]{y, x});
         }
 
         // Down & Left
-        x = X;
-        y = Y;
-        while (x > 0 && y > 0) {
+        x = original_x;
+        y = original_y;
+        while (x > 1 && y > 1) {
             x--;
             y--;
-            move = new ChessMove(new ChessPosition(x + 1, y + 1), new ChessPosition(x, y), null);
+            move = new ChessMove(originalPosition, new ChessPosition(y, x), null);
             validMoves.add(move);
+            coordinates.add(new int[]{y, x});
+        }
+
+        // Up & Left
+        x = original_x;
+        y = original_y;
+        while (x > 1 && y < 8) {
+            x--;
+            y++;
+            move = new ChessMove(originalPosition, new ChessPosition(y, x), null);
+            validMoves.add(move);
+            coordinates.add(new int[]{y, x});
         }
 
 //        new int[][]{
-//                {6, 5}, {7, 6}, {8, 7},
-//                {4, 5}, {3, 6}, {2, 7}, {1, 8},
-//                {4, 3}, {3, 2}, {2, 1},
-//                {6, 3}, {7, 2}, {8, 1},
+//                {6, 5}, {7, 6}, {8, 7}, up right
+//                {4, 5}, {3, 6}, {2, 7}, {1, 8}, down right
+//                {4, 3}, {3, 2}, {2, 1}, down left
+//                {6, 3}, {7, 2}, {8, 1}, up left
 //        }
 
-
-        System.out.println(validMoves.toArray().length);
         return validMoves;
     }
     /*

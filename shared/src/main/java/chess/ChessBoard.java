83244cc05c96630ddf2f82481ee2c9a78aad2094
dallinmj <dallinmjacobs@gmail.com>
2024-10-02 18:28:08 -0600

    implemented deepCopy

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -42,8 +42,16 @@ public class ChessBoard {
     /**
      * Creates a deep copy of the current board
      */
-    public ChessBoard deepCopy() {
-        throw new RuntimeException("Not implemented");
+    public ChessBoard deepCopy(ChessBoard board) {
+        ChessBoard copy =  new ChessBoard();
+        for (int i = 1; i <= 8; i++) { // y values
+            for (int j = 1; j <= 8; j++) { // x values
+                ChessPosition position = new ChessPosition(j, i);
+                ChessPiece oldPiece = board.getPiece(position);
+                copy.addPiece(position, oldPiece);
+            }
+        }
+        return copy;
     }
 
     @Override

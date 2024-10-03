d3d47f2e6597cc5c1b5429731e98c492ad8d3a90
dallinmj <dallinmjacobs@gmail.com>
2024-10-02 19:29:47 -0600

    Completed validMove tests

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -41,6 +42,8 @@ public class ChessGame {
         BLACK
     }
 
+//    Expected :[{row=5, col=6}{row=5, col=5}null, {row=5, col=6}{row=5, col=4}null, {row=5, col=6}{row=5, col=3}null, {row=5, col=6}{row=5, col=2}null, {row=5, col=6}{row=5, col=7}null]
+//    Actual   :[{row=5, col=6}{row=6, col=6}null, {row=5, col=6}{row=5, col=5}null, {row=5, col=6}{row=8, col=6}null, {row=5, col=6}{row=5, col=4}null, {row=5, col=6}{row=7, col=6}null, {row=5, col=6}{row=5, col=3}null, {row=5, col=6}{row=5, col=2}null, {row=5, col ...
     /**
      * Gets a valid moves for a piece at the given location
      *
@@ -52,12 +55,11 @@ public class ChessGame {
         // Get potential moves for the given piece
         ChessPiece piece = board.getPiece(startPosition);
         Collection<ChessMove> potentialMoves = piece.pieceMoves(board, startPosition);
-        TeamColor teamColor = piece.getTeamColor();
 
         // Filter out moves that would put the team in check
-        Collection<ChessMove> validMoves = potentialMoves;
+        Collection<ChessMove> validMoves = new ArrayList<>(potentialMoves);
             // Iterate through potential moves and remove those that would put the team in check
-        for (ChessMove move: potentialMoves) {
+        for (ChessMove move : potentialMoves) {
             // Test the move, throw exception if invalid
             try {
                 makeMove(move, true);
@@ -76,7 +78,6 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move, Boolean testMove) throws InvalidMoveException {
-        ChessBoard copy = board.deepCopy();
         ChessPiece piece = board.getPiece(move.startPosition());
         ChessPiece replacedPiece = board.getPiece(move.endPosition());
 
@@ -113,18 +114,37 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        Collection<ChessPiece> enemyPieces = new ArrayList<>(); // Calculate moves for each piece and add all of them to a massive list, see if any of those = kingPosition :)
-        Collection<ChessPosition> enemyPotentialPositions = new ArrayList<>();
+        // Calculate moves for each piece and add all of them to a massive list, see if any of those = kingPosition :)
+        Collection<ChessMove> enemyPotentialMoves = new ArrayList<>();
+        ChessPosition kingPosition = null;
         boolean inCheck = false;
 
         // Iterate through all values to find the king and to calculate all enemy moves
-        for (int i = 0; i < 8; i++){ // y values
-            for (int j = 0; j < 8; j++){ // x values
+        for (int i = 1; i <= 8; i++){ // y values
+            for (int j = 1; j <= 8; j++){ // x values
                 ChessPosition position = new ChessPosition(i, j);
                 ChessPiece piece = board.getPiece(position);
+
+                if (piece == null){ continue; }
+
+                // Get king position
                 if (piece.getPieceType() == ChessPiece.PieceType.KING && piece.getTeamColor() == teamColor){
-                    ChessPosition kingPosition = position;
+                    kingPosition = position;
                 }
+
+                // Get enemy moves
+                if (piece.getTeamColor() != teamColor){
+                    Collection<ChessMove> moves = piece.pieceMoves(board, position);
+                    enemyPotentialMoves.addAll(moves);
+                }
+            }
+        }
+
+        // Calculate if in check
+        for (ChessMove move : enemyPotentialMoves){
+            if (move.endPosition().equals(kingPosition)) {
+                inCheck = true;
+                break;
             }
         }
 

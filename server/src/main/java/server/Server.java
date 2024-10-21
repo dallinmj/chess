182c38ed415eafc67309eea1aea308959f92c158
dallinmj <dallinmjacobs@gmail.com>
2024-10-21 17:07:50 -0600

    Added server functionality

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,12 @@
 package server;
 
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryGameDAO;
+import dataAccess.MemoryUserDAO;
+import server.handlers.GameHandler;
+import server.handlers.UserHandler;
+import service.GameService;
+import service.UserService;
 import spark.*;
 
 public class Server {
@@ -9,7 +16,22 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
+        MemoryGameDAO memoryGameDAO = new MemoryGameDAO();
+        MemoryAuthDAO memoryAuthDAO = new MemoryAuthDAO();
+        MemoryUserDAO memoryUserDAO = new MemoryUserDAO();
+
         // Register your endpoints and handle exceptions here.
+        UserService userService = new UserService(memoryAuthDAO, memoryUserDAO, memoryGameDAO);
+        GameService gameService = new GameService(memoryAuthDAO, memoryUserDAO, memoryGameDAO);
+
+        UserHandler userHandler = new UserHandler(userService);
+        GameHandler gameHandler = new GameHandler(gameService);
+
+        Spark.post("/user", userHandler::register);
+        Spark.post("/session", userHandler::login);
+        Spark.delete("/session", userHandler::logout);
+        Spark.post("/game", gameHandler::createGame);
+        Spark.get("/game", gameHandler::listGames);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();

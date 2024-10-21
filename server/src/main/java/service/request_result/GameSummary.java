728b99ccc75cbffb5c855a6e69aa28871329ce64
dallinmj <dallinmjacobs@gmail.com>
2024-10-21 17:10:53 -0600

    game summary class

--- server/src/main/java/service/request_result/GameSummary.java
+++ server/src/main/java/service/request_result/GameSummary.java
@@ -1,4 +1,32 @@
 package service.request_result;
 
 public class GameSummary {
+    private final int gameId;
+    private final String whiteUsername;
+    private final String blackUsername;
+    private final String gameName;
+
+    public GameSummary(int gameId, String whiteUsername, String blackUsername, String gameName) {
+        this.gameId = gameId;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
 }

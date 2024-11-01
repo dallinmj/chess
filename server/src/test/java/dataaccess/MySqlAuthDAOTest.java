2f72ca8f2d845a766fb51254bf5c5856d3439ada
dallinmj <dallinmjacobs@gmail.com>
2024-10-31 19:48:56 -0600

    Implemented most of AuthDAO

--- server/src/test/java/dataaccess/MySqlAuthDAOTest.java
+++ server/src/test/java/dataaccess/MySqlAuthDAOTest.java
@@ -1,6 +1,7 @@
 package dataaccess;
 
 import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,26 +10,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class MySqlAuthDAOTest {
 
     MySqlAuthDAO mySqlAuthDAO;
+    MySqlUserDAO mySqlUserDAO;
 
     @BeforeEach
     void setUp() throws DataAccessException {
         mySqlAuthDAO = new MySqlAuthDAO();
+        mySqlUserDAO = new MySqlUserDAO();
+        DatabaseManager databaseManager = new DatabaseManager();
+        databaseManager.configureDatabase();
+        mySqlAuthDAO.clearAllAuth();
+        mySqlUserDAO.clearAllUsers();
     }
 
     @Test
     void createAuth() throws DataAccessException {
-        DatabaseManager databaseManager = new DatabaseManager();
-        databaseManager.configureDatabase();
-        AuthData authData = new AuthData("authToken123", "username123");
+        mySqlUserDAO.createUser(new UserData("diggy", "password123", "email@123"));
+        AuthData authData = new AuthData("authToken123", "diggy");
         mySqlAuthDAO.createAuth(authData);
     }
 
     @Test
-    void getAuth() {
+    void getAuth() throws DataAccessException {
+        mySqlUserDAO.createUser(new UserData("dallinmj", "monkey", "dal@gmail.com"));
+        mySqlAuthDAO.createAuth(new AuthData("authydal", "dallinmj"));
+        System.out.println(mySqlAuthDAO.getAuth("authydal"));
     }
 
     @Test
-    void deleteAuth() {
+    void deleteAuth() throws DataAccessException {
+        mySqlUserDAO.createUser(new UserData("deletemeauth", "ohno", "delete@me.com"));
+        AuthData authData = new AuthData("deleteauth", "deletemeauth");
+
+        mySqlAuthDAO.createAuth(authData);
+        System.out.println(mySqlAuthDAO.getAuth(authData.authToken()));
+        mySqlAuthDAO.deleteAuth(authData);
+        System.out.println(mySqlAuthDAO.getAuth(authData.authToken()));
     }
 
     @Test
@@ -36,7 +52,15 @@ class MySqlAuthDAOTest {
     }
 
     @Test
-    void listAuths() {
+    void listAuths() throws DataAccessException {
+        mySqlUserDAO.createUser(new UserData("user1", "pass1", "email1"));
+        mySqlUserDAO.createUser(new UserData("user2", "pass2", "email2"));
+        mySqlUserDAO.createUser(new UserData("user3", "pass3", "email3"));
+        mySqlAuthDAO.createAuth(new AuthData("auth1", "user1"));
+        mySqlAuthDAO.createAuth(new AuthData("auth2", "user2"));
+        mySqlAuthDAO.createAuth(new AuthData("auth3", "user3"));
+
+        System.out.println(mySqlAuthDAO.listAuths());
     }
 
     @Test

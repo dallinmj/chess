a92f981b8c7dc5d937283d9ed8f12f3b3aed4bb3
dallinmj <dallinmjacobs@gmail.com>
2024-11-12 22:44:43 -0700

    Making tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,26 @@
 package client;
 
+import dataaccess.DataAccessException;
+import network.ServerFacade;
 import org.junit.jupiter.api.*;
 import server.Server;
+import service.requestresult.ClearRequest;
+import service.requestresult.gamerequestresult.CreateGameRequest;
+import service.requestresult.gamerequestresult.JoinGameRequest;
+import service.requestresult.userrequestresult.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -20,10 +28,45 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    public void clear() throws DataAccessException {
+         ServerFacade.clear(new ClearRequest());
+    }
+
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void loginTest() throws DataAccessException {
+        ServerFacade.register(new RegisterRequest("doug", "goodpassword", "Diggyemail"));
+        LoginRequest request = new LoginRequest("doug", "goodpassword");
+        var result = ServerFacade.login(request);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void registerTest() throws DataAccessException {
+        RegisterRequest request = new RegisterRequest("doug", "goodpassword", "Diggyemail");
+        var result = ServerFacade.register(request);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void logoutTest() throws DataAccessException {
+        ServerFacade.register(new RegisterRequest("doug", "goodpassword", "Diggyemail"));
+        LoginResult loginResult = ServerFacade.login(new LoginRequest("doug", "goodpassword"));
+        String auth = loginResult.authToken();
+
+        LogoutRequest logoutRequest = new LogoutRequest(auth);
+        var result = ServerFacade.logout(logoutRequest);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void createGameTest() throws DataAccessException {
+        RegisterResult registerResult = ServerFacade.register(new RegisterRequest("doug", "goodpassword", "Diggyemail"));
+        String auth = registerResult.authToken();
+        CreateGameRequest createGameRequest = new CreateGameRequest(auth, "gamename!");
+        var createGameResult = ServerFacade.createGame(createGameRequest);
+        Assertions.assertNotNull(createGameResult);
     }
 
 }

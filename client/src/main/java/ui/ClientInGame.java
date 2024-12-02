0a1cb554b54a5ab72e276946e246e3072e9206ac
dallinmj <dallinmjacobs@gmail.com>
2024-12-02 16:46:54 -0700

    creating InGame client

--- client/src/main/java/ui/ClientInGame.java
+++ client/src/main/java/ui/ClientInGame.java
@@ -1,4 +1,56 @@
 package ui;
 
-public class ClientInGame {
+import chess.ChessBoard;
+
+import java.util.Arrays;
+
+public class ClientInGame implements Client{
+
+    private final ChessBoard board;
+
+    public ClientInGame(ChessBoard board) {
+        this.board = board;
+    }
+
+    @Override
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "redraw" -> redraw();
+                case "leave" -> leave();
+                case "move" -> move(params);
+                case "resign" -> resign();
+                case "highlight" -> highlight(params);
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println(help());
+    }
+
+    private String help() {
+        return """
+                Commands:
+                help
+                redraw
+                leave
+                move <from> <to>
+                resign
+                highlight <piece>""";
+    }
+
+    private String highlight(String... params) {
+
+        return "highlight";
+    }
+
+
 }

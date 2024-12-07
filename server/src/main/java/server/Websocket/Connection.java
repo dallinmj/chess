80f5244a8d994cb29fd07b7c4622092d536be304
dallinmj <dallinmjacobs@gmail.com>
2024-12-06 17:36:42 -0700

    Connection classes

---
+++ server/src/main/java/server/Websocket/Connection.java
@@ -0,0 +1,29 @@
+package server.Websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+
+public class Connection {
+    public String auth;
+    public int gameId;
+    public Session session;
+
+    @Override
+    public String toString() {
+        return "Connection{" +
+                "auth='" + auth + '\'' +
+                ", gameId=" + gameId +
+                '}';
+    }
+
+    public Connection(String auth, int gameId, Session session) {
+        this.auth = auth;
+        this.gameId = gameId;
+        this.session = session;
+    }
+
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
+}
---
+++ server/src/main/java/server/Websocket/ConnectionManager.java
@@ -0,0 +1,43 @@
+package server.Websocket;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String auth, int gameId, Session session) {
+        var connection = new Connection(auth, gameId, session);
+        connections.put(auth, connection);
+    }
+
+    public void remove(String auth) {
+        connections.remove(auth);
+    }
+
+    public void broadcast(String excludeAuth, int gameId, ServerMessage message) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        System.out.println("Broadcasting message: " + message.toString());
+        System.out.println(connections.values());
+        for (var c : connections.values()) {
+            System.out.println("connection: " + c.auth + " " + c.gameId + " Excluded Auth: " + excludeAuth);
+            if (c.session.isOpen()) {
+                if (!c.auth.equals(excludeAuth) && c.gameId == gameId) {
+                    c.send(new Gson().toJson(message));
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var c : removeList) {
+            connections.remove(c.auth);
+        }
+    }
+}
